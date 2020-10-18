1665cc8543a4362a2beb47d2793cb20dac66e71c
Kristýna Říhová <rihokristyn@gmail.com>
2020-10-18 14:33:35 +0200

    pozdrav a první hra

---
+++ skript.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+vstup = int(input("Zadej číslo: "))
+if vstup > 100:
+    print("Hej, to číslo je fakt velké :-D")
+elif vstup > 10:
+    print("střední číslo")
+else:
+    print(f'tohle že je číslo?')
