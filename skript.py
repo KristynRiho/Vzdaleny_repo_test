77359ca7702ebd4f489ed0dc5f7184bb894f6a78
Kristýna Říhová <rihokristyn@gmail.com>
2020-10-18 18:42:06 +0200

    třetí hra

--- skript.py
+++ skript.py
@@ -1,3 +1,4 @@
+import random
 print("Hello World!")
 vstup = int(input("Zadej číslo: "))
 if vstup > 100:
@@ -12,3 +13,11 @@ print("Druhá hra!")
 vstup = input("Zadej jakékoliv slovo:")
 delka_slova = len(vstup)
 print(f'Délka tvého slova je {delka_slova} znaků.')
+
+# třetí hra:
+print(f'Myslím si nějaké číslo mezi 1 a 6')
+vstup = int(input("Hádej, jaké číslo si myslím v rozmezí od 1 do 6:"))
+nahodne_cislo = random.randint(1, 6)
+while vstup != nahodne_cislo:
+    vstup = int(input("Zkus to znovu, vyber jiné číslo v rozmezí od 1 do 6:"))
+print(f'Super, trefa!')
